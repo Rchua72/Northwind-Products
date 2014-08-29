33be266988af1610a91fb4124f90e95a30aefb00
Rchua72 <Rhchua@gmail.com>
2014-08-29 14:11:01 +1000

    Changed from Ninject to SimpleInjector

---
+++ NorthwindProducts/App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,39 @@
+[assembly: WebActivator.PostApplicationStartMethod(typeof(NorthwindProducts.App_Start.SimpleInjectorInitializer), "Initialize")]
+
+namespace NorthwindProducts.App_Start
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using SimpleInjector;
+    using SimpleInjector.Extensions;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Integration.Web.Mvc;
+    using Northwind.Data;
+    
+    public static class SimpleInjectorInitializer
+    {
+        /// <summary>Initialize the container and register it as MVC3 Dependency Resolver.</summary>
+        public static void Initialize()
+        {
+            // Did you know the container can diagnose your configuration? Go to: https://bit.ly/YE8OJj.
+            var container = new Container();
+            
+            InitializeContainer(container);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            
+            container.Verify();
+            
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+     
+        private static void InitializeContainer(Container container)
+        {
+            container.Register<IApplicationUnit, ApplicationUnit>();
+
+            // For instance:
+            // container.Register<IUserRepository, SqlUserRepository>();
+        }
+    }
+}
